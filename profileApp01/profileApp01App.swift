5de11cc3d6d40fd6543558151aa5165d8bd2c4bf
Vinit Jha <jhavinit01@gmail.com>
2023-03-08 20:43:29 +0530

    Initial Commit

---
+++ profileApp01/profileApp01App.swift
@@ -0,0 +1,17 @@
+//
+//  profileApp01App.swift
+//  profileApp01
+//
+//  Created by Vinit Jha on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct profileApp01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
